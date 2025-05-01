410646d429c1eb36eab9fe40ea90b5c97da9bba1
abdul <abdullahabdeldayem007@gmail.com>
2025-05-01 07:43:15 +0300

    fix

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<loadmore_listview/LoadmoreListviewPlugin.h>)
+#import <loadmore_listview/LoadmoreListviewPlugin.h>
+#else
+@import loadmore_listview;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LoadmoreListviewPlugin registerWithRegistrar:[registry registrarForPlugin:@"LoadmoreListviewPlugin"]];
+}
+
+@end
